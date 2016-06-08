028d33451f4c8e68aca4c1aba2601ae6f05cbad4
Bradley Rosenfeld <bradley@rosenfeld.xyz>
2016-06-08 17:20:29 -0500

    Updated icon color

--- index.php
+++ index.php
@@ -23,28 +23,29 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 		<link rel="stylesheet" href="css/main.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="apple-mobile-web-app-capable" content="yes">
-		<link rel="apple-touch-icon" sizes="57x57" href="/RosenfeldVPN/icons/apple-touch-icon-57x57.png">
-		<link rel="apple-touch-icon" sizes="60x60" href="/RosenfeldVPN/icons/apple-touch-icon-60x60.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="/RosenfeldVPN/icons/apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="/RosenfeldVPN/icons/apple-touch-icon-76x76.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="/RosenfeldVPN/icons/apple-touch-icon-114x114.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="/RosenfeldVPN/icons/apple-touch-icon-120x120.png">
-		<link rel="apple-touch-icon" sizes="144x144" href="/RosenfeldVPN/icons/apple-touch-icon-144x144.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="/RosenfeldVPN/icons/apple-touch-icon-152x152.png">
-		<link rel="apple-touch-icon" sizes="180x180" href="/RosenfeldVPN/icons/apple-touch-icon-180x180.png">
-		<link rel="icon" type="image/png" href="/RosenfeldVPN/icons/favicon-32x32.png" sizes="32x32">
-		<link rel="icon" type="image/png" href="/RosenfeldVPN/icons/android-chrome-192x192.png" sizes="192x192">
-		<link rel="icon" type="image/png" href="/RosenfeldVPN/icons/favicon-96x96.png" sizes="96x96">
-		<link rel="icon" type="image/png" href="/RosenfeldVPN/icons/favicon-16x16.png" sizes="16x16">
-		<link rel="manifest" href="/RosenfeldVPN/icons/manifest.json">
-		<link rel="mask-icon" href="/RosenfeldVPN/icons/safari-pinned-tab.svg" color="#501616">
-		<link rel="shortcut icon" href="/RosenfeldVPN/icons/favicon.ico">
+		<link rel="apple-touch-icon" sizes="57x57" href="/RosenfeldVPN/icons/apple-touch-icon-57x57.png?v=bOO82xW3dp">
+		<link rel="apple-touch-icon" sizes="60x60" href="/RosenfeldVPN/icons/apple-touch-icon-60x60.png?v=bOO82xW3dp">
+		<link rel="apple-touch-icon" sizes="72x72" href="/RosenfeldVPN/icons/apple-touch-icon-72x72.png?v=bOO82xW3dp">
+		<link rel="apple-touch-icon" sizes="76x76" href="/RosenfeldVPN/icons/apple-touch-icon-76x76.png?v=bOO82xW3dp">
+		<link rel="apple-touch-icon" sizes="114x114" href="/RosenfeldVPN/icons/apple-touch-icon-114x114.png?v=bOO82xW3dp">
+		<link rel="apple-touch-icon" sizes="120x120" href="/RosenfeldVPN/icons/apple-touch-icon-120x120.png?v=bOO82xW3dp">
+		<link rel="apple-touch-icon" sizes="144x144" href="/RosenfeldVPN/icons/apple-touch-icon-144x144.png?v=bOO82xW3dp">
+		<link rel="apple-touch-icon" sizes="152x152" href="/RosenfeldVPN/icons/apple-touch-icon-152x152.png?v=bOO82xW3dp">
+		<link rel="apple-touch-icon" sizes="180x180" href="/RosenfeldVPN/icons/apple-touch-icon-180x180.png?v=bOO82xW3dp">
+		<link rel="icon" type="image/png" href="/RosenfeldVPN/icons/favicon-32x32.png?v=bOO82xW3dp" sizes="32x32">
+		<link rel="icon" type="image/png" href="/RosenfeldVPN/icons/favicon-194x194.png?v=bOO82xW3dp" sizes="194x194">
+		<link rel="icon" type="image/png" href="/RosenfeldVPN/icons/favicon-96x96.png?v=bOO82xW3dp" sizes="96x96">
+		<link rel="icon" type="image/png" href="/RosenfeldVPN/icons/android-chrome-192x192.png?v=bOO82xW3dp" sizes="192x192">
+		<link rel="icon" type="image/png" href="/RosenfeldVPN/icons/favicon-16x16.png?v=bOO82xW3dp" sizes="16x16">
+		<link rel="manifest" href="/RosenfeldVPN/icons/manifest.json?v=bOO82xW3dp">
+		<link rel="mask-icon" href="/RosenfeldVPN/icons/safari-pinned-tab.svg?v=bOO82xW3dp" color="#333333">
+		<link rel="shortcut icon" href="/RosenfeldVPN/icons/favicon.ico?v=bOO82xW3dp">
 		<meta name="apple-mobile-web-app-title" content="rVPN">
 		<meta name="application-name" content="rVPN">
-		<meta name="msapplication-TileColor" content="#da532c">
-		<meta name="msapplication-TileImage" content="/RosenfeldVPN/icons/mstile-144x144.png">
-		<meta name="msapplication-config" content="/RosenfeldVPN/icons/browserconfig.xml">
-		<meta name="theme-color" content="#501616">
+		<meta name="msapplication-TileColor" content="#ffc40d">
+		<meta name="msapplication-TileImage" content="/RosenfeldVPN/icons/mstile-144x144.png?v=bOO82xW3dp">
+		<meta name="msapplication-config" content="/RosenfeldVPN/icons/browserconfig.xml?v=bOO82xW3dp">
+		<meta name="theme-color" content="#333333">
 	</head>
 	<body>
 		<header>
